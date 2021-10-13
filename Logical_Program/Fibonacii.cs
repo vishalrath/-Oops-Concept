72d842a82d0d9a36a05ad14a2227757d7beb2de3
vishal <vishalrathod7038@gmail.com>
2021-10-13 07:50:11 +0530

    logical Problem Assignment is done

---
+++ Logical_Program/Fibonacii.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Program
+{
+    class Fibonacii
+    {
+        public void fibo()
+        {
+
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.Write("Enter the number of elements: ");
+            number = int.Parse(Console.ReadLine());
+            //printing 0 and 1
+            Console.Write(n1 + " " + n2 + " ");
+            //loop starts from 2 because 0 and 1 are already printed  
+            for (i = 2; i < number; ++i)          
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }  
+}
